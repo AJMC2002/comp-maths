1963d951f3794fd4dbaf81ba101bd33ca91188ad
AJMC2002 <adrianmosquera2015@gmail.com>
2023-11-01 00:23:08 +1000

    edited variables to make it readable

--- Labs/5-sem/richardson-method/app/Main.hs
+++ Labs/5-sem/richardson-method/app/Main.hs
@@ -5,7 +5,9 @@ import MyLib
 
 main :: IO ()
 main = do
-  let x1 = richardson (V3 (V3 (2.0 :: Double) 1.0 0.0) (V3 0.0 2.0 1.0) (V3 1.0 0.0 3.0)) (V3 2.0 1.0 4.0) (V3 0.0 0.0 0.0) 0.2
-  let x2 = richardson (V3 (V3 (2.0 :: Double) 1.0 0.0) (V3 0.0 2.0 1.0) (V3 1.0 0.0 3.0)) (V3 2.0 1.0 4.0) (V3 0.0 0.0 0.0) 0.5
-  print x1
-  print x2
+  let a = V3 (V3 (2.0 :: Double) 1.0 0.0) (V3 0.0 2.0 1.0) (V3 1.0 0.0 3.0)
+  let b = V3 2.0 1.0 4.0
+  let x0 = zero V3
+  let omega = 0.2
+  let x = richardson a b x0 omega
+  print x
